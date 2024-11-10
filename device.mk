5e7862cfb8a5da1e2595880fbf7356ad3d6408e1
Volodymyr Zhdanov <wight554@gmail.com>
2024-11-10 13:54:14 -0500

    malmo: Build IPACM from source

--- device.mk
+++ device.mk
@@ -185,6 +185,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.malmo
