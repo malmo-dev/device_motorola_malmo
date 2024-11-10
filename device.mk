aa271df24a31d76788b375b9ee55dfd7089443c1
sabrina010209 <mcpe11640@gmail.com>
2024-11-10 14:49:18 -0500

    malmo: Build vendor service manager

--- device.mk
+++ device.mk
@@ -359,6 +359,11 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/telephony_system-ext_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/telephony_system-ext_privapp-permissions-qti.xml
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservice \
+    vndservicemanager
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-V1-ndk.vendor \
